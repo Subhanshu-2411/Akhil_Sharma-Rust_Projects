dba26b43bcbdfb274f8a3baa40140730f693bf1b
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-18 01:02:10 +0530

    Rust API Calls Done

--- Rust_Get_Request/src/main.rs
+++ Rust_Get_Request/src/main.rs
@@ -4,12 +4,12 @@ use std::io::Read;
 error_chain! {
     foreign_links {
         Io(std::io::Error);
-        HttpRequest(request::Error);
+        HttpRequest(reqwest::Error);
     }
 }
 
 fn main() -> Result<()> {
-    let mut res = request::blocking::get("http://httpbin.org/get")?;
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
 
